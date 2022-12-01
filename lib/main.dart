ead3d17ac4c0559ad641b65d727954e9668c7482
Brice.Baguette <Brice.Baguette@student.uliege.be>
2022-12-01 11:10:41 +0100

    Correct inventory close button

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:mobile_kombat/firebase_options.dart';
 import 'package:mobile_kombat/models/loader.dart';
 import 'package:mobile_kombat/views/game_scene.dart';
 import 'package:mobile_kombat/views/loading_screen.dart';
@@ -14,6 +16,9 @@ import 'views/inventory.dart';
 import 'views/shop.dart';
 
 Future main() async {
+  /*await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );*/
   WidgetsFlutterBinding.ensureInitialized();
   await SystemChrome.setPreferredOrientations([
     DeviceOrientation.landscapeLeft,
--- lib/models/auth.dart
+++ lib/models/auth.dart
@@ -1,5 +1,23 @@
-class Auth {
-  signIn(String email, String password) {}
+import 'package:firebase_auth/firebase_auth.dart';
 
-  signOut(String email, String password) {}
+class Auth {
+  signUp(String email, String password) async {
+    try {
+      final credential =
+          await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        print('The password provided is too weak.');
+      } else if (e.code == 'email-already-in-use') {
+        print('The account already exists for that email.');
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  signIn(String email, String password) {}
 }
